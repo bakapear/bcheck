36071da79267734f8ea51aa4998b45a6973a4136
bakapear <proxybomb@gmail.com>
2022-04-21 18:03:00 +0200

    update zlog tools

--- tools/zlog/genConfig.js
+++ tools/zlog/genConfig.js
@@ -1,24 +1,32 @@
 let ITERS = 100
-let DELAY = 0.4
+let DELAY = 0.2
 
 let PREFIX = 'z'
 let NEXT = 'zlog_next'
 let CMD = 'sm_zlog'
 
-let i = 0
+let i = 1
 let num = x => x < 10 ? '0' + x : x
+
 let add = (a, b, c) => {
   CFG.push(`alias ${PREFIX}_${num(i)} "alias ${NEXT} ${PREFIX}_${num(++i)}; ${CMD} ${a} ${b} ${c} ${ITERS}"`)
 }
+let echo = (a, b) => {
+  CFG.push('', `alias ${PREFIX}_${num(i)} "hud_reloadscheme; wait 500; ${PREFIX}_${num(++i)}; wait 50; con_logfile zlog_${a}-${b}.log"`, '')
+}
 
-let CFG = [`alias ${NEXT} ${PREFIX}_${num(i)}`]
+let CFG = ['sv_allow_wait_command 1', 'cl_autoreload 0', 'sdr_spew_level 0', `alias ${NEXT} ${PREFIX}_${num(i)}`]
 
 let DIRS = ['N', 'F', 'B', 'L', 'R', 'FL', 'FR', 'BL', 'BR']
 let TYPES = ['S', 'DS', 'JS', 'JDS']
 
-for (let type of TYPES) {
-  CFG.push('', '// HOLD ' + type)
+let MOD = 'WA'
+if (MOD) {
+  DIRS = DIRS.map(x => MOD + x)
+}
 
+for (let type of TYPES) {
+  echo('HOLD', type)
   for (let dir of DIRS) {
     let start = type === 'DS' ? dir + 'D' : dir
     add(start, DELAY, dir + type)
@@ -26,7 +34,7 @@ for (let type of TYPES) {
 }
 
 for (let type of TYPES) {
-  CFG.push('', '// TICK ' + type)
+  echo('TICK', type)
 
   for (let dir of DIRS) {
     let start = type === 'DS' ? 'ND' : 'N'
@@ -35,13 +43,15 @@ for (let type of TYPES) {
 }
 
 for (let type of TYPES) {
-  CFG.push('', '// REVERSE ' + type)
+  echo('REVERSE', type)
 
   for (let dir of DIRS) {
     let start = type === 'DS' ? dir + 'D' : dir
     add(start, DELAY, type)
   }
 }
+
+CFG.push('', `alias ${PREFIX}_${num(i)} "wait 50; con_logfile console.log"`)
 
 CFG.push('', NEXT, '')
 
--- tools/zlog/readme.txt
+++ tools/zlog/readme.txt
@@ -2,7 +2,5 @@
 2. Put zlog.cfg in your config files
 3. Start local server (-insecure with zlog plugin blabla)
 4. Join server & spawn as Soldier with primary equipped
-5. con_logfile zlog_<launcher>.log
-6. exec zlog.cfg
-8. Stay tabbed in game for best results
-9. Once it finished do con_logfile "" to finish writing to log
+5. exec zlog.cfg
+6. Stay tabbed in game for best results
